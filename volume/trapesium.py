ace80ae0c09de246063b01793e7b7879f6978f93
regalileo <regalnugraha93@gmail.com>
2024-05-26 14:06:06 +0700

    Create trapesium.py

---
+++ volume/trapesium.py
@@ -0,0 +1,6 @@
+def volume_trapesium():
+    alas_atas = int(input("Masukan Nilai Alas_Atas :"))
+    alas_bawah = int(input("Masukan Nilai Alas_Bawah :"))
+    tinggi = int(input("Masukan Nilai Tinggi :"))
+    panjang = int(input("Masukan Nilai Panjang :"))
+    return 1/2 * (alas_bawah + alas_atas) * tinggi * panjang
